64ce590bec037089531a9cf0703d4b361a171420
Andrew Pargeter <drew@devicedriver.com>
2023-08-20 12:19:08 -0600

    Initial Commit.

---
+++ main/switch_led_queue.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+
+#define INPUT_PIN 4
+#define LED_PIN 2
+
+int state = 0;
+xQueueHandle interuptQueue;
+
+static void IRAM_ATTR gpio_interrupt_handler(void *args)
+{
+    int pinNumber = (int)args;
+    
+    xQueueSendFromISR(interuptQueue, &pinNumber, NULL);
+}
+
+void LED_Control_Task(void *params)
+{
+    int pinNumber, count = 0;
+    while (true)
+    {
+        if (xQueueReceive(interuptQueue, &pinNumber, 1000 / portTICK_PERIOD_MS)) //portMAX_DELAY))
+        {
+            printf("GPIO %d was pressed %d times. The state is %d\n", pinNumber, count++,
+                    gpio_get_level(INPUT_PIN));
+            gpio_set_level(LED_PIN, gpio_get_level(INPUT_PIN));
+        }
+        //else
+        //{
+            if(gpio_get_level(INPUT_PIN)==0)
+            {
+                printf("GPIO set to 0\n");
+                gpio_set_level(LED_PIN, 0);
+            }
+        //}
+            	
+    }
+}
+
+void app_main(void)
+{
+    gpio_pad_select_gpio(LED_PIN);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    gpio_pad_select_gpio(INPUT_PIN);
+    gpio_set_direction(INPUT_PIN, GPIO_MODE_INPUT);
+    gpio_pulldown_en(INPUT_PIN);
+    gpio_pullup_dis(INPUT_PIN);
+    gpio_set_intr_type(INPUT_PIN, GPIO_INTR_POSEDGE);
+
+    interuptQueue = xQueueCreate(10, sizeof(int));
+    xTaskCreate(LED_Control_Task, "LED_Control_Task", 2048, NULL, 1, NULL);
+
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(INPUT_PIN, gpio_interrupt_handler, (void *)INPUT_PIN);
+}
---
+++ main/switch_led_tasknotify.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+#define INPUT_PIN 4
+#define LED_PIN 2
+
+static const char *TAG = "switch_led:";
+
+volatile long interruptCount = 0;
+
+//int state = 0;
+static TaskHandle_t recvTaskHandle = NULL;
+#define LED_MASK 1
+
+//static TaskHandle_t testTaskHandle = NULL;
+
+static void IRAM_ATTR gpio_interrupt_handler(void *args)
+{
+    interruptCount++;
+    
+    if(gpio_get_level(INPUT_PIN))
+        xTaskNotifyFromISR(recvTaskHandle, LED_MASK, eSetValueWithOverwrite, NULL);
+    else
+        xTaskNotifyFromISR(recvTaskHandle, 0, eSetValueWithOverwrite, NULL);
+    
+}
+
+/*
+void Test_Task(void *params) {
+
+  while (1) {
+    ESP_LOGI(TAG, "Test_Task: notify'ing");
+    xTaskNotify(recvTaskHandle, LED_MASK, eSetValueWithOverwrite);
+    vTaskDelay(3000 / portTICK_RATE_MS);
+  }
+
+}
+*/
+void LED_Control_Task(void *params) {
+  int count = 0;
+  
+  while (true) {
+    ESP_LOGI(TAG, "LED_Control_Task: calling ulTaskNotifyTake()");
+    uint32_t notificationValue = ulTaskNotifyTake(pdTRUE, portMAX_DELAY); //(10*1000) / portTICK_RATE_MS);//portMAX_DELAY);
+    ESP_LOGI(TAG, "ulTaskNotifyTake notificationValue = %d", notificationValue);
+    if (notificationValue == LED_MASK) {
+      ESP_LOGI(TAG, "GPIO %d is pressed %d. The state is %d", INPUT_PIN, count++, gpio_get_level(INPUT_PIN));
+      gpio_set_level(LED_PIN, gpio_get_level(INPUT_PIN));
+    } else ESP_LOGI(TAG, "GPIO %d is NOT pressed %d. The state is %d", INPUT_PIN, count++, gpio_get_level(INPUT_PIN));
+    gpio_set_level(LED_PIN, gpio_get_level(INPUT_PIN));
+
+    vTaskDelay(3000 / portTICK_RATE_MS);
+
+  }
+}
+
+void app_main(void) {
+  gpio_pad_select_gpio(LED_PIN);
+  gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+  gpio_pad_select_gpio(INPUT_PIN);
+  gpio_set_direction(INPUT_PIN, GPIO_MODE_INPUT);
+  gpio_pulldown_en(INPUT_PIN);
+  gpio_pullup_dis(INPUT_PIN);
+  gpio_set_intr_type(INPUT_PIN, GPIO_INTR_POSEDGE);
+
+  xTaskCreate(LED_Control_Task, "LED_Control_Task", 2048, NULL, 1, &recvTaskHandle);
+
+  vTaskDelay(1000 / portTICK_RATE_MS);
+  //xTaskCreate(Test_Task, "Test_Task", 2048, NULL, 1, &testTaskHandle);
+
+
+  gpio_install_isr_service(0);
+  gpio_isr_handler_add(INPUT_PIN, gpio_interrupt_handler, (void *)INPUT_PIN);
+ 
+  while(1){
+     vTaskDelay(3000 / portTICK_RATE_MS);
+     ESP_LOGI(TAG, "Interrupt Count = %ld\n", interruptCount);
+
+     if(!gpio_get_level(INPUT_PIN))
+       gpio_set_level(LED_PIN, 0);
+  }
+
+}
